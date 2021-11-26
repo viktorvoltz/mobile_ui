c44cfb450ef6a36a63e6e1b3736cf7954144f147
viktorvoltz <vctrchinyeaka@gmail.com>
2021-11-26 16:49:09 +0100

    ui changes

--- lib/widgets/Body2.dart
+++ lib/widgets/Body2.dart
@@ -64,11 +64,49 @@ class Body2 extends StatelessWidget {
               ),
               SizedBox(width: 20,),
               Container(
-                margin: EdgeInsets.only(right: 15),
+                margin: EdgeInsets.only(left: 15),
+                padding: EdgeInsets.only(left: 10, top: 5),
                 height: 150,
                 width: 180,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(15), color: task[1].theme),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(task[1].icon, color: kRedDark, size: 27,),
+                      SizedBox(height: 40,),
+                      Text(task[1].title, style: TextStyle(fontWeight: FontWeight.w700, color: Colors.black, fontSize: 16),),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(top: 15),
+                            alignment: Alignment.center,
+                            height: 30,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              color: kRedDark,
+                              borderRadius: BorderRadius.circular(20)
+                            ),
+                            child: Text(task[1].left, style: TextStyle(color: task[1].theme, fontWeight: FontWeight.w600),),
+                          ),
+                          SizedBox(width: 10),
+                          Container(
+                            margin: EdgeInsets.only(top: 5),
+                            alignment: Alignment.center,
+                            height: 30,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20)
+                            ),
+                            child: Text(task[1].done, style: TextStyle(color: kRed, fontWeight: FontWeight.w600),),
+                          )
+                        ],
+                      )
+
+                    ],
+                  ),
               ),
             ],
           )
@@ -114,4 +152,53 @@ class GridCard extends StatelessWidget {
       ),
     );
   }
+}
+
+Widget containerCards (Color iconColor, Color textColor, int index, ){
+  return 
+              Container(
+                margin: EdgeInsets.only(left: 15),
+                padding: EdgeInsets.only(left: 10, top: 5),
+                height: 150,
+                width: 180,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15), color: task[0].theme),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Icon(task[0].icon, color: kYellowDark, size: 27,),
+                      SizedBox(height: 40,),
+                      Text(task[0].title, style: TextStyle(fontWeight: FontWeight.w700, color: Colors.black, fontSize: 16),),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.only(top: 15),
+                            alignment: Alignment.center,
+                            height: 30,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              color: kYellowDark,
+                              borderRadius: BorderRadius.circular(20)
+                            ),
+                            child: Text(task[0].left, style: TextStyle(color: task[0].theme, fontWeight: FontWeight.w600),),
+                          ),
+                          SizedBox(width: 10),
+                          Container(
+                            margin: EdgeInsets.only(top: 5),
+                            alignment: Alignment.center,
+                            height: 30,
+                            width: 70,
+                            decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(20)
+                            ),
+                            child: Text(task[0].done, style: TextStyle(color: kYellowDark, fontWeight: FontWeight.w600),),
+                          )
+                        ],
+                      )
+
+                    
+                    ],),
+              );
 }
