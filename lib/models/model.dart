57997c43711297bfdf47fa97655067e154535223
viktorvoltz <vctrchinyeaka@gmail.com>
2021-11-28 21:22:13 +0100

    ui changes

--- lib/models/model.dart
+++ lib/models/model.dart
@@ -2,7 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:management_ui/constants/colors.dart';
 
 class Task {
-  const Task({required this.title, this.icon, this.theme, required this.left, required this.done});
+  const Task(
+      {required this.title,
+      this.icon,
+      this.theme,
+      required this.left,
+      required this.done});
   final String title;
   final IconData? icon;
   final Color? theme;
@@ -11,8 +16,23 @@ class Task {
 }
 
 const List<Task> task = <Task>[
-  const Task(title: 'Personal', icon: Icons.person, theme: kYellow, left: '3 left', done: '1 done'),
-  const Task(title: 'Work', icon: Icons.work, theme: kRed, left: '2 left', done: '8 done'),
-  const Task(title: 'Health', icon: Icons.health_and_safety, theme: kBlue, left: '1 left', done: '1 done'),
+  const Task(
+      title: 'Personal',
+      icon: Icons.person,
+      theme: kYellow,
+      left: '3 left',
+      done: '1 done'),
+  const Task(
+      title: 'Work',
+      icon: Icons.work,
+      theme: kRed,
+      left: '2 left',
+      done: '8 done'),
+  const Task(
+      title: 'Health',
+      icon: Icons.health_and_safety,
+      theme: kBlue,
+      left: '1 left',
+      done: '1 done'),
   //const Task(title: 'Add', icon: Icons.add, theme: kYellowLight),
 ];
--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -9,37 +9,47 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
         backgroundColor: Colors.white,
-        elevation: 0,
-        title: Row(
-          children: [
-            Container(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Row(
+            children: [
+             /* Container(
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.black,
+                ),
                 height: 45,
                 width: 50,
                 margin: EdgeInsets.only(left: 0),
-                child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: Image.asset('assets/images/spacex.jpg'))),
-            SizedBox(width: 20),
-            Text(
-              'Welcome, Voltz!',
-              style: TextStyle(color: Colors.black),
-            ),
+                child: Image.asset('assets/images/spacex.jpg'),
+              ),*/
+              CircleAvatar(
+                radius: 25.0,
+                backgroundImage:
+                    AssetImage('assets/images/spacex.jpg'),
+                backgroundColor: Colors.transparent,
+              ),
+              SizedBox(width: 20),
+              Text(
+                'Welcome, Voltz!',
+                style: TextStyle(color: Colors.black),
+              ),
+            ],
+          ),
+          actions: [
+            Icon(Icons.more_vert, color: Colors.black),
           ],
         ),
-        actions: [
-          Icon(Icons.more_vert, color: Colors.black),
-        ],
-      ),
-      body: Column(
-        children: [
-          Body1(),
-          SizedBox(height: 10,),
-          Body2(),
-        ],
-      )
-    );
+        body: Column(
+          children: [
+            Body1(),
+            SizedBox(
+              height: 10,
+            ),
+            Body2(),
+          ],
+        ));
   }
 }
