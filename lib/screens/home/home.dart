1fbd3573bd0942f0abd639781c4bdee2f84409b6
viktorvoltz <vctrchinyeaka@gmail.com>
2021-12-07 00:49:24 +0100

    floating botton

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -50,36 +50,55 @@ class HomePage extends StatelessWidget {
           Body2(),
         ],
       ),
-
       bottomNavigationBar: _bottomNavigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10)
+        ),
+        elevation: 0,
+        backgroundColor: Colors.black,
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          size: 35,
+        ),
+      ),
     );
   }
 }
 
-Widget _bottomNavigationBar(){
+Widget _bottomNavigationBar() {
   return Container(
     decoration: BoxDecoration(
-      borderRadius: BorderRadius.only(
-        topLeft: Radius.circular(30),
-        topRight: Radius.circular(30),
-      ),
-      boxShadow: [
-        BoxShadow(
-          color: Colors.grey.withOpacity(0.2),
-          spreadRadius: 5,
-          blurRadius: 10
-        )
-      ]
-    ),
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        boxShadow: [
+          BoxShadow(
+              color: Colors.grey.withOpacity(0.2),
+              spreadRadius: 5,
+              blurRadius: 10)
+        ]),
     child: ClipRRect(
       borderRadius: BorderRadius.only(
         topLeft: Radius.circular(30),
         topRight: Radius.circular(30),
       ),
       child: BottomNavigationBar(
+        backgroundColor: Colors.white,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        selectedItemColor: Colors.blueAccent,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
         items: [
-          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home_rounded, size: 30)),
-          BottomNavigationBarItem(label: 'person', icon: Icon(Icons.person, size: 30),),
+          BottomNavigationBarItem(
+              label: 'Home', icon: Icon(Icons.home_rounded, size: 30)),
+          BottomNavigationBarItem(
+            label: 'person',
+            icon: Icon(Icons.person, size: 30),
+          ),
         ],
       ),
     ),
