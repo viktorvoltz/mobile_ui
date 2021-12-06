236e7e6ba24dc5021d0384556ba53be65513c72c
viktorvoltz <vctrchinyeaka@gmail.com>
2021-12-07 00:41:21 +0100

    bottom bar

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -9,13 +9,13 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.white,
-          elevation: 0,
-          title: Row(
-            children: [
-             /* Container(
+        elevation: 0,
+        title: Row(
+          children: [
+            /* Container(
                 decoration: BoxDecoration(
                   shape: BoxShape.circle,
                   color: Colors.black,
@@ -25,31 +25,63 @@ class HomePage extends StatelessWidget {
                 margin: EdgeInsets.only(left: 0),
                 child: Image.asset('assets/images/spacex.jpg'),
               ),*/
-              CircleAvatar(
-                radius: 25.0,
-                backgroundImage:
-                    AssetImage('assets/images/spacex.jpg'),
-                backgroundColor: Colors.transparent,
-              ),
-              SizedBox(width: 20),
-              Text(
-                'Welcome, Voltz!',
-                style: TextStyle(color: Colors.black),
-              ),
-            ],
-          ),
-          actions: [
-            Icon(Icons.more_vert, color: Colors.black),
+            CircleAvatar(
+              radius: 25.0,
+              backgroundImage: AssetImage('assets/images/spacex.jpg'),
+              backgroundColor: Colors.transparent,
+            ),
+            SizedBox(width: 20),
+            Text(
+              'Welcome, Voltz!',
+              style: TextStyle(color: Colors.black),
+            ),
           ],
         ),
-        body: Column(
-          children: [
-            Body1(),
-            SizedBox(
-              height: 10,
-            ),
-            Body2(),
-          ],
-        ));
+        actions: [
+          Icon(Icons.more_vert, color: Colors.black),
+        ],
+      ),
+      body: Column(
+        children: [
+          Body1(),
+          SizedBox(
+            height: 10,
+          ),
+          Body2(),
+        ],
+      ),
+
+      bottomNavigationBar: _bottomNavigationBar(),
+    );
   }
+}
+
+Widget _bottomNavigationBar(){
+  return Container(
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(30),
+        topRight: Radius.circular(30),
+      ),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.grey.withOpacity(0.2),
+          spreadRadius: 5,
+          blurRadius: 10
+        )
+      ]
+    ),
+    child: ClipRRect(
+      borderRadius: BorderRadius.only(
+        topLeft: Radius.circular(30),
+        topRight: Radius.circular(30),
+      ),
+      child: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home_rounded, size: 30)),
+          BottomNavigationBarItem(label: 'person', icon: Icon(Icons.person, size: 30),),
+        ],
+      ),
+    ),
+  );
 }
